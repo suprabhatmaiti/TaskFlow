2c80843e7a72e990ab05965b6eb39b5986d8287e
Suprabhat Maiti <maitisuprabhat@gmail.com>
2025-11-24 20:15:59 +0530

    add database connection setup using pg and dotenv

---
+++ src/server/config/db.js
@@ -0,0 +1,13 @@
+import { Pool } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+pool.connect().then(() => console.log("Connected to PostgreSQL"));
