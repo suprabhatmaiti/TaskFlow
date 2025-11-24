136e0c2812aeb8099dc1bd3e3e692bbf35b272f3
Suprabhat Maiti <maitisuprabhat@gmail.com>
2025-11-24 20:18:20 +0530

    feat: set up authentication routes for user registration, login, and token management

--- src/server/main.js
+++ src/server/main.js
@@ -1,12 +1,21 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/hello", (req, res) => {
   res.send("Hello Vite + React!");
 });
 
+app.use("/api/auth", authRoutes);
+
 ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+  console.log("Server is listening on port 3000...")
 );
---
+++ src/server/routes/authRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  refreshToken,
+  register,
+  login,
+  logout,
+} from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+
+router.post("/login", login);
+router.post("/refresh-token", refreshToken);
+router.post("/logout", logout);
+
+export default router;
