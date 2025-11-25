871562346afc431da0ef7cd1a5140b5969b72608
Suprabhat Maiti <maitisuprabhat@gmail.com>
2025-11-25 11:42:20 +0530

    feat: update dependencies and implement routing with authentication and toast notifications

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -1,12 +1,43 @@
-import "./index.css";
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import { AuthProvider } from "./hook/useAuth.jsx";
+import AuthPage from "./pages/AuthPage/index.jsx";
+import DashboardPage from "./pages/DashboardPage/index.jsx";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import PublicRoute from "./components/PublicRoute.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: (
+          <PublicRoute>
+            <AuthPage />
+          </PublicRoute>
+        ),
+      },
+      {
+        path: "/dashboard",
+        element: (
+          <ProtectedRoute>
+            <DashboardPage />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
 );
